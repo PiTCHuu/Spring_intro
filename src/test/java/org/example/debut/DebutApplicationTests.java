9f5b0679c0da50bd7c9166950d458f9957430740
Amboara <amboaraniainarkt@gmail.com>
2026-03-11 17:28:50 +0300

    first commit

---
+++ src/test/java/org/example/debut/DebutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.debut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
